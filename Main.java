a6a1b12e6a67e69e84119a71e467961b87f11cc1
leogsurf <95723886+ImLeoFR@users.noreply.github.com>
2022-09-08 17:00:46 +0000

    Atualização

--- Main.java
+++ Main.java
@@ -10,12 +10,13 @@ class Main {
     System.out.println("Qual o valos de dias totais?");
     diasto = scan.nextInt ();
 
+    anos = (diasto /365);
+    meses = (diasto/30);
 
-    anos = diasto / 365;
+    System.out.println("Sua idade em dias é de " + diasto + ", sua idade em meses e de " + meses + " e sua idade em anos é de " + anos + ".");
+
+
     
-    double anosf = (double)  (Math.round(anos*1.0) / 1.0);
-
-    System.out.println(anosf);
 
     
   }
